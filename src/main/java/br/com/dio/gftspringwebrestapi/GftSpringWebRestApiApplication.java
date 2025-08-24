03e5090b4bf4ad7f03631a787c8dcc7212b34efd
GhortheBrute <143045110+GhortheBrute@users.noreply.github.com>
2025-08-23 21:23:42 -0300

    First commit

---
+++ src/main/java/br/com/dio/gftspringwebrestapi/GftSpringWebRestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.gftspringwebrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GftSpringWebRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GftSpringWebRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/dio/gftspringwebrestapi/GftSpringWebRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.gftspringwebrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GftSpringWebRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
