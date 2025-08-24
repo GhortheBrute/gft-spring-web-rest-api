ba73197b487bee1a310b4a4079f27df68501d27c
GhortheBrute <143045110+GhortheBrute@users.noreply.github.com>
2025-08-23 21:33:18 -0300

    First Endpoint.

---
+++ src/main/java/br/com/dio/gftspringwebrestapi/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package br.com.dio.gftspringwebrestapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome to Spring Boot REST API!";
+    }
+}
